f0a60c6d55553f2923fe04653781aadb80ec912a
Ignacio Cortorreal <luis3ignacio@hotmail.com>
2013-07-14 17:03:10 +0000

    modified web.js to read index.html

--- web.js
+++ web.js
@@ -4,11 +4,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
-    if (err) throw err;
-    //console.log(data);
-    response.send(data);
-  });
+  var data = fs.readFileSync('index.html');
+  if (err) throw err;
+  //console.log(data);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
